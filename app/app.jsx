ff5b980c9e167653fe0597245dc917d48a5afa11
philip <philip.clarke@redhood.co.uk>
2016-12-28 15:08:25 +0000

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hasHistory} = require('react-router');
+var Main = require('Main');
+
+
+  ReactDOM.render(
+  <Router history={hasHistory}>
+    <Route path="/" component={Main}>
+    </Route>
+  </Router>,
+  document.getElementById('app')
+  );
