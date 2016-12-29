89bd7c2acdb3785edb143cd54242ae0dc1baf1ab
philip <philip.clarke@redhood.co.uk>
2016-12-29 15:23:42 +0000

    added foundation

--- app/app.jsx
+++ app/app.jsx
@@ -5,7 +5,11 @@ var Main = require('Main');
 var Weather = require('Weather');
 var About = require('About');
 var Examples = require('Examples');
+//load foundation
+require('style!css!foundation-sites/dist/foundation.min.css')
+$(document).foundation();
 import { hashHistory } from 'react-router'
+
 
   ReactDOM.render(
   <Router history={hashHistory}>
