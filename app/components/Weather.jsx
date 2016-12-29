eb6b408cae860849e4e72ce56e733b14cb5f2777
philip <philip.clarke@redhood.co.uk>
2016-12-29 12:25:58 +0000

    setting up debugger

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -5,30 +5,45 @@ var openWeatherApp = require('openWeatherApp');
 var Weather = React.createClass({
   getInitialState: function(){
     return{
-      location: 'London',
-      temp: 88
+      isLoading: false
     }
   },
   handleSearch: function(location){
     var that = this;
+    debugger;
+    this.setState({isLoading: true});
     openWeatherApp.getTemp(location).then(function (temp){
       that.setState({
         location: location,
         temp: temp,
+        isLoading : false,
       })
     }, function(err) {
       alert(err);
+      that.setState({
+        isLoading : false
+      });
     })
 
   },
   render: function(){
     var temp = this.state.temp;
     var location = this.state.location;
+    var isLoading = this.state.isLoading;
+
+    function renderMessage(){
+      if (isLoading == true){
+        return <h3>Fetching Weather ...</h3>;
+      } else if (temp && location) {
+        return <WeatherMessage temp={temp} location={location}></WeatherMessage>
+
+      }
+    }
     return (
       <div>
       <h3>Get Weather</h3>
       <WeatherForm onSearch={this.handleSearch}></WeatherForm>
-      <WeatherMessage temp={temp} location={location}></WeatherMessage>
+      {renderMessage()}
       </div>
     );
   }
--- app/components/WeatherMessage.jsx
+++ app/components/WeatherMessage.jsx
@@ -4,7 +4,6 @@ var WeatherMessage = React.createClass({
   render: function(){
     var temp = this.props.temp;
     var location = this.props.location;
-    console.log(this.props.location);
     return (
       <div>
         <p>The Temperature in {location} is {temp}</p>
