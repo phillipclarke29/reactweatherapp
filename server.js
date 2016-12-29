d0e5dd931e0eae103f616f4a74c4b75ce7339ae7
philip <philip.clarke@redhood.co.uk>
2016-12-29 14:43:42 +0000

    refactored server for heroku

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.port||3000;
+
+  app.use(function(req, res, next){
+    if(req.headers['x-forwarded-proto']=== 'http'){
+      next();
+    } else {
+      res.redirect('http://' +req.hostname + req.url);
+    }
+  });
 
   app.use(express.static('public'));
 
-  app.listen(3000, function(){
-    console.log ('hello from your server on port 3000');
-
-
+  app.listen(PORT, function(){
+    console.log ('hello from your server on port ' + PORT);
   });
