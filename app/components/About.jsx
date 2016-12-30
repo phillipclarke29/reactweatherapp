ba4c7b4ef9978842370530f978a19af542e10813
philip <philip.clarke@redhood.co.uk>
2016-12-30 06:58:01 +0000

    added about text

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,14 @@ var React = require('react');
 var About = (props)=> {
   return (
     <div>
-    <h3>About Component</h3>
-    <p>Welcome to the about Page</p>
+    <h1>About</h1>
+    <p>This add is about exploring REACT using the Open Weather API</p>
+    <li>
+    <a href="https://www.udemy.com/the-complete-react-web-app-developer-course">Learn React</a>
+    </li>
+    <li>
+    <a href="https://openweathermap.org/api">Open Weather</a>
+    </li>
     </div>
 )
 };
